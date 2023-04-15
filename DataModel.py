3300d4c2786293e7add531cc19e1b73e73998bc5
Nicolás Klopstock Triana <77993013+nklopstock611@users.noreply.github.com>
2023-04-14 23:37:59 -0500

    Update DataModel.py

--- DataModel.py
+++ DataModel.py
@@ -1,14 +1,11 @@
 from pydantic import BaseModel
 
 class DataModel(BaseModel):
-
-# Estas varibles permiten que la librería pydantic haga el parseo entre el Json recibido y el modelo declarado.
     selling_price: int
     year: float
     km_driven: float
     max_power: float
     transmission: str
 
-# Esta función retorna los nombres de las columnas correspondientes con el modelo exportado en joblib.
     def columns(self):
         return ['selling_price', 'year', 'km_driven', 'max_power', 'transmission']
